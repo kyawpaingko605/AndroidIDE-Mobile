c6e5a41c96aa6851f34e7f36acd04a1d55df7780
kyawpaingko605 <kyawpaingko605@gmail.com>
2026-05-28 15:43:14 +0700

    Update IDEBuildConfigProviderImplTest.kt

--- core/app/src/test/java/com/itsaky/androidide/app/configuration/IDEBuildConfigProviderImplTest.kt
+++ core/app/src/test/java/com/itsaky/androidide/app/configuration/IDEBuildConfigProviderImplTest.kt
@@ -1,5 +1,5 @@
 /*
- *  This file is part of AndroidIDE.
+ *  TThis file is part of AndroidIDE.
  *
  *  AndroidIDE is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
